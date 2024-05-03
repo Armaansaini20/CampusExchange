8fc4d1015889950f7a6718384a58bf064bf41f77
Armaansaini20 <113324811+Armaansaini20@users.noreply.github.com>
2024-05-03 15:38:56 +0530

    finall

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -2,16 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://campus-exchange.onrender.com/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[]);
+
   return (
-    <div className="App">
-
-
-      <Header />
-      <Home />
-    </div>
+  <div className="App">
+    <h1>{message}</h1>
+  </div>
   );
 }
 
 export default App;
+
