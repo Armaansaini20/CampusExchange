504c7cef0502a6c3bc99787477ad373222da41e9
Armaansaini20 <113324811+Armaansaini20@users.noreply.github.com>
2024-05-08 12:29:39 +0530

    meoww

--- node-app/index.js
+++ node-app/index.js
@@ -9,8 +9,8 @@ const userController = require('./controllers/userController');
 const corsOptions = {
     origin: "https://663a5740a8fc512dab273ce0--ornate-hummingbird-795016.netlify.app./", // frontend URI (ReactJS)
 }
-app.use(express.json());
-app.use(cors(corsOptions));
+//app.use(express.json());
+//app.use(cors(corsOptions));
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads')
